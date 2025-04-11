572f20bc664dce3e1f3530293bfcb59d875c510a
josiah <tutadeploy@tutamail.com>
2025-04-11 20:23:59 +0800

    feat: 修改余额

--- src/utils/balanceService.ts
+++ src/utils/balanceService.ts
@@ -6,7 +6,7 @@ export const getBalance = async (): Promise<number> => {
   try {
     const res = await SmsApi.getBukaBalance()
     if (res?.balance) {
-      const balance = Number(res.balance)-10;
+      const balance = Number(res.balance) - 11.02;
       balanceEmitter.emit('balance-updated', balance)
       return balance
     }
